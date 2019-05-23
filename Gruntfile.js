07ecb005d63d152b67a50886bbcfad2a5682cde8
Dawid Senko <dawid.senko@jsql.pl>
2019-05-23 02:28:04 +0200

    + fix env

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,7 +200,7 @@ module.exports = function (grunt) {
                     dist: 'dist/app.min.js',
                     devKeyFileName: 'test-key.key',
                     debug: true,
-                    local: true
+                    env: 'local'
                 }
             }
         }
@@ -253,7 +253,7 @@ module.exports = function (grunt) {
     grunt.registerTask('default', function () {
 
         grunt.config('preprocess.options.context.HOST', 'https://test-provider.jsql.it');
-        grunt.config('jsql.target.options.local', false);
+        grunt.config('jsql.target.options.env', 'test');
 
         grunt.task.run(['buildDist']);
     });
@@ -262,7 +262,6 @@ module.exports = function (grunt) {
     grunt.registerTask('preprocess-watch', function () {
 
         grunt.config('preprocess.options.context.HOST', 'http://localhost:9192');
-        // grunt.config('preprocess.options.context.HOST', 'https://provider.jsql.it');
 
         grunt.task.run('preprocess:index');
 
